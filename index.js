8de29c4cea06786a4c410469f234403e3b4e5810
github username <github email address>
2019-10-01 10:46:15 -0400

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ function map(src, fn) {
      }
      return newArr
 }
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
