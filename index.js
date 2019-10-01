d78aa9a1d23cf099fdbf4c519c32c721b40ed2bb
github username <github email address>
2019-10-01 10:45:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Add your functions here
+
+function map(src, fn) {
+    let newArr = []
+    for (let i = 0; i < src.length; i++) {
+        const element = fn(src[i])
+        newArr.push(element)
+     }
+     return newArr
+}
