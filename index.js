cd284602a9297c5cc0c0f225f422bc2ebbf693c0
Mic Fox <michael.c.fox.ii@gmail.com>
2019-10-02 19:18:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(array, callback) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++){
+        let element = array[i]
+        newArray.push(callback(element))
+    }
+    return newArray
+}
+
+function reduce(array, callback, start) {
+    let truthy = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+
+    for (; i < array.length; i++) {
+        truthy = callback(array[i], truthy)
+    }
+    return truthy
+}
