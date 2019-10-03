acfbab3585615f2020c6f7ef2ee7548be80a196f
Evan_Ratzlaff <ratzlaff92@gmail.com>
2019-10-03 09:08:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(someArray, callback){
+    let newArray = []
+    for (let i = 0; i < someArray.length; i++){
+        let element = array[i]
+        newArray.push(callback(element))
+    }
+    return newArray
+}
+
+function reduce(array, callback, start){
+    let cup = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+    for (; i < array.length; i++) {
+        cup = callback(array[i], cup)
+    }
+    return cup
+}
