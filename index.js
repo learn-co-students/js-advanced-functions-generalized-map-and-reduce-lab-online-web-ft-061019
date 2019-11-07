1b20e7fdb07a3e98d78f031bf62404c279a9e86e
latinawhocodes <alyssaroque01@gmail.com>
2019-11-07 17:08:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Add your functions here
+function map(array, func){
+    return array.map(func)
+}
+
+function reduce(array, func, initial){
+    if (!!initial == false){
+        initial = array.shift();
+    }
+    return array.reduce(func, initial)
+}
